9a2f0c0e695767b2df6e74a8c3121d07fef40a6d
Mario <mario@fameuchner.de>
2026-01-20 20:14:05 +0100

    Update server.ts

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,5 +1,5 @@
 import http from "http";
-import { routeRequest } from "./routes.js";
+import { routeRequest } from "./routes/index.js";
 
 export function createServer() {
   return http.createServer((req, res) => {
