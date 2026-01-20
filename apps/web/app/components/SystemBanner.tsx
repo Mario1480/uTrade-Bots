83b49ddb8a8270a30da4d690a5f0e18f20cd2595
Mario <mario@fameuchner.de>
2026-01-20 08:19:37 +0100

    Update SystemBanner.tsx

--- apps/web/app/components/SystemBanner.tsx
+++ apps/web/app/components/SystemBanner.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { apiGet } from "../lib/api";
+import { apiGet } from "../../lib/api";
 
 type SystemSettings = {
   tradingEnabled: boolean;
